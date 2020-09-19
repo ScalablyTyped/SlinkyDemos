00fac74aaaf8f75d4d89ce3fcb5b04c33a789ecc
Mario Càllisto <mario.callisto@gmail.com>
2020-09-19 15:11:29 +0200

    Experimenting with Stack.Navigator #27

--- react-navigation/src/main/scala/hello/world/App.scala
+++ react-navigation/src/main/scala/hello/world/App.scala
@@ -2,16 +2,26 @@ package hello.world
 
 import slinky.core._
 import slinky.core.annotations.react
-import slinky.core.facade.Hooks.useState
-import slinky.core.facade.ReactElement
-import slinky.native.{ScrollView, Text, View}
-
+import slinky.native.{Text, View}
+import typings.reactNavigationCore.anon.InitialRouteName
+import typings.reactNavigationStack.mod.createStackNavigator
+import typings.reactNavigationCore.typesMod.TypedNavigator
 @react object App {
 
   type Props = Unit
 
   val component = FunctionalComponent[Props] { _ =>
 
+    val stack: TypedNavigator[_, _, _, _, _] = createStackNavigator
+
+    val initial: InitialRouteName[_] = InitialRouteName().setInitialRouteName("Home")
+
+    val navigator: ReactComponentClass[_] = stack.Navigator
+
+//    val screen = stack.Screen(_underscore = ???)
+
     View(Text("Whatever"))
+
   }
+
 }

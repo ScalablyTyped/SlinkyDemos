585ca7f753b136e2eaeab320f4138d4969d4a7c9
mcallisto <mario.callisto@gmail.com>
2020-09-20 12:00:32 +0200

    Demonstrate Expo Constants (#29)
    * Empty Expo constants
    
    * Added expo-constants library
    
    * Correct location

--- react-native/src/main/scala/hello/world/Home.scala
+++ react-native/src/main/scala/hello/world/Home.scala
@@ -3,6 +3,7 @@ package hello.world
 import slinky.core.FunctionalComponent
 import slinky.core.annotations.react
 import slinky.native.{Text, View}
+import typings.expoConstants.mod.default.{expoVersion, statusBarHeight}
 import typings.reactNative.mod.{TextStyle, ViewStyle}
 
 @react object Home {
@@ -14,7 +15,9 @@ import typings.reactNative.mod.{TextStyle, ViewStyle}
         Text(style = TextStyle().setFontSize(16))(
           "This is a demo written in Scala through Scala.js, Slinky, ScalablyTyped and Expo.\n\n" +
             "It uses components from Antd Native and React Router Native."
-        )
+        ),
+        Text(s"The status bar is ${statusBarHeight} pixels high."),
+        Text(s"Expo version is ${expoVersion}.")
       )
   }
 }

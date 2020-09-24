e24ae3e2688d05bd48569544d0821ced0fcd6616
Mario Càllisto <mario.callisto@gmail.com>
2020-09-24 13:21:37 +0200

    Basic demo

--- react-native/src/main/scala/hello/world/PlayAudio.scala
+++ react-native/src/main/scala/hello/world/PlayAudio.scala
@@ -2,8 +2,10 @@ package hello.world
 
 import slinky.core.FunctionalComponent
 import slinky.core.annotations.react
-import slinky.native.{Text, View}
-import typings.expoAv.aVMod.{AVPlaybackStatus, AVPlaybackStatusToSet}
+import slinky.native.View
+import typings.antDesignReactNative.components._
+import typings.antDesignReactNative.{antDesignReactNativeStrings => antdStrings}
+import typings.expoAv.aVMod.AVPlaybackStatusToSet
 import typings.expoAv.mod.Audio.Sound
 import typings.expoAv.anon.Headers
 
@@ -14,17 +16,38 @@ import scala.concurrent.ExecutionContext.Implicits.global
   type Props = Unit
 
   val component = FunctionalComponent[Props] { _ =>
-    val uri: String = "https://upload.wikimedia.org/wikipedia/commons/e/e4/En-us-tough.ogg"
-
+    val uri: String = "https://upload.wikimedia.org/wikipedia/commons/b/b2/Funky_bassline_F7-Bb7.ogg"
     val soundObject = new Sound
+
     soundObject
       .loadAsync(
         Headers(uri = uri),
-        AVPlaybackStatusToSet().setShouldPlay(true) // optional, but you can set a bunch of stuff here
+        AVPlaybackStatusToSet().setShouldPlay(false) // optional, but you can set a bunch of stuff here
       )
       .toFuture
-      .flatMap((_: AVPlaybackStatus) => soundObject.playAsync().toFuture)
 
-    View(Text("tough"))
+    View(
+      WhiteSpace().size(antdStrings.md),
+      Flex(
+        Flex(
+          Button(Icon(name = "play-circle"))
+            .`type`(antdStrings.primary)
+            .onPress(_ => soundObject.playAsync().toFuture),
+          Text("Play")
+        ).direction(antdStrings.column),
+        Flex(
+          Button(Icon(name = "pause-circle"))
+            .`type`(antdStrings.primary)
+            .onPress(_ => soundObject.pauseAsync().toFuture),
+          Text("Pause")
+        ).direction(antdStrings.column),
+        Flex(
+          Button(Icon(name = "stop"))
+            .`type`(antdStrings.primary)
+            .onPress(_ => soundObject.stopAsync().toFuture),
+          Text("Stop")
+        ).direction(antdStrings.column)
+      ).justify(antdStrings.around)
+    )
   }
 }

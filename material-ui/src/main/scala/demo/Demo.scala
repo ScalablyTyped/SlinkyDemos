437a18a9bc6481e5714dbe6d0c2bb1570f224984
Alexis Hernandez <alexis22229@gmail.com>
2020-10-23 13:20:33 -0600

    material-ui: Use the react-router-dom switch
    This prevents that several components get rendered at the same time,
    the switch will render the first matching component.

--- material-ui/src/main/scala/demo/Demo.scala
+++ material-ui/src/main/scala/demo/Demo.scala
@@ -19,7 +19,7 @@ import typings.materialUiCore.typographyTypographyMod.Style
 import typings.materialUiIcons.{components => Icon}
 import typings.materialUiStyles.components.ThemeProvider
 import typings.reactRouter.mod.RouteProps
-import typings.reactRouterDom.components.{BrowserRouter, Link, Route}
+import typings.reactRouterDom.{components => router}
 
 object Demo {
 
@@ -38,112 +38,114 @@ object Demo {
   val Main: FunctionalComponent[Props] = FunctionalComponent[Props] {
     case () =>
       ThemeProvider(theme)(
-        BrowserRouter.basename(basename)(
-          Route(
-            RouteProps()
-              .setExact(true)
-              .setPath("/")
-              .setRender(_ =>
-                List(
-                  ListSubheader.inset(true)(""),
-                  Link[String](to = "/dashboard")(
-                    ListItem.button(true)(ListItemIcon(Icon.Assignment()), ListItemText.primary("Dashboard"))
-                  ),
-                  Link[String](to = "/album")(
-                    ListItem.button(true)(ListItemIcon(Icon.Assignment()), ListItemText.primary("Album"))
-                  ),
-                  Link[String](to = "/signin")(
-                    ListItem.button(true)(ListItemIcon(Icon.Assignment()), ListItemText.primary("Sign In"))
-                  ),
-                  Link[String](to = "/login")(
-                    ListItem.button(true)(ListItemIcon(Icon.Assignment()), ListItemText.primary("Login"))
-                  ),
-                  Link[String](to = "/button")(
-                    ListItem.button(true)(ListItemIcon(Icon.Assignment()), ListItemText.primary("Buttons"))
-                  ),
-                  Link[String](to = "/select")(
-                    ListItem.button(true)(ListItemIcon(Icon.Assignment()), ListItemText.primary("Select"))
-                  ),
-                  Link[String](to = "/customization")(
-                    ListItem.button(true)(ListItemIcon(Icon.Assignment()), ListItemText.primary("Customization"))
+        router.BrowserRouter.basename(basename)(
+          router.Switch(
+            router.Route(
+              RouteProps()
+                .setExact(true)
+                .setPath("/")
+                .setRender(_ =>
+                  List(
+                    ListSubheader.inset(true)(""),
+                    router.Link[String](to = "/dashboard")(
+                      ListItem.button(true)(ListItemIcon(Icon.Assignment()), ListItemText.primary("Dashboard"))
+                    ),
+                    router.Link[String](to = "/album")(
+                      ListItem.button(true)(ListItemIcon(Icon.Assignment()), ListItemText.primary("Album"))
+                    ),
+                    router.Link[String](to = "/signin")(
+                      ListItem.button(true)(ListItemIcon(Icon.Assignment()), ListItemText.primary("Sign In"))
+                    ),
+                    router.Link[String](to = "/login")(
+                      ListItem.button(true)(ListItemIcon(Icon.Assignment()), ListItemText.primary("Login"))
+                    ),
+                    router.Link[String](to = "/button")(
+                      ListItem.button(true)(ListItemIcon(Icon.Assignment()), ListItemText.primary("Buttons"))
+                    ),
+                    router.Link[String](to = "/select")(
+                      ListItem.button(true)(ListItemIcon(Icon.Assignment()), ListItemText.primary("Select"))
+                    ),
+                    router.Link[String](to = "/customization")(
+                      ListItem.button(true)(ListItemIcon(Icon.Assignment()), ListItemText.primary("Customization"))
+                    )
                   )
                 )
-              )
-          ),
-          Route(
-            RouteProps()
-              .setPath("/dashboard")
-              .setRender(_ =>
-                Fragment(
-                  AppTheme(
-                    title = "Dashboard page layout example - Material-UI",
-                    description = "An example layout for creating an albumn.",
-                    hideCredit = true
-                  )(Dashboard())
+            ),
+            router.Route(
+              RouteProps()
+                .setPath("/dashboard")
+                .setRender(_ =>
+                  Fragment(
+                    AppTheme(
+                      title = "Dashboard page layout example - Material-UI",
+                      description = "An example layout for creating an albumn.",
+                      hideCredit = true
+                    )(Dashboard())
+                  )
                 )
-              )
-          ),
-          Route(
-            RouteProps()
-              .setPath("/album")
-              .setRender(_ =>
-                Fragment(
-                  AppTheme(
-                    title = "Album page layout - Material-UI",
-                    description = "An example layout for creating an album or gallery."
-                  )(Album())
+            ),
+            router.Route(
+              RouteProps()
+                .setPath("/album")
+                .setRender(_ =>
+                  Fragment(
+                    AppTheme(
+                      title = "Album page layout - Material-UI",
+                      description = "An example layout for creating an album or gallery."
+                    )(Album())
+                  )
                 )
-              )
-          ),
-          Route(
-            RouteProps()
-              .setPath("/signin")
-              .setRender(_ =>
-                Fragment(
-                  AppTheme(
-                    title = "Sign-in page layout example - Material-UI",
-                    description = "An example layout for creating a sign-in page."
-                  )(SignIn())
+            ),
+            router.Route(
+              RouteProps()
+                .setPath("/signin")
+                .setRender(_ =>
+                  Fragment(
+                    AppTheme(
+                      title = "Sign-in page layout example - Material-UI",
+                      description = "An example layout for creating a sign-in page."
+                    )(SignIn())
+                  )
                 )
-              )
-          ),
-          Route(
-            RouteProps()
-              .setPath("/login")
-              .setRender(_ => Login())
-          ),
-          Route(
-            RouteProps()
-              .setPath("/button")
-              .setRender(_ =>
-                Fragment(
-                  Typography.variant(Style.h4).gutterBottom(true).component("h2")("Buttons"),
-                  ButtonTest("dear user"),
-                  StyledButtonDemo(),
-                  StyledButtonHooksDemo()
+            ),
+            router.Route(
+              RouteProps()
+                .setPath("/login")
+                .setRender(_ => Login())
+            ),
+            router.Route(
+              RouteProps()
+                .setPath("/button")
+                .setRender(_ =>
+                  Fragment(
+                    Typography.variant(Style.h4).gutterBottom(true).component("h2")("Buttons"),
+                    ButtonTest("dear user"),
+                    StyledButtonDemo(),
+                    StyledButtonHooksDemo()
+                  )
                 )
-              )
-          ),
-          Route(
-            RouteProps()
-              .setPath("/select")
-              .setRender(_ =>
-                Fragment(
-                  Typography.variant(Style.h4).gutterBottom(true).component("h2")("Select"),
-                  SelectDemo(scala.List("one", "two", "three"))
+            ),
+            router.Route(
+              RouteProps()
+                .setPath("/select")
+                .setRender(_ =>
+                  Fragment(
+                    Typography.variant(Style.h4).gutterBottom(true).component("h2")("Select"),
+                    SelectDemo(scala.List("one", "two", "three"))
+                  )
                 )
-              )
-          ),
-          Route(
-            RouteProps()
-              .setPath("/customization")
-              .setRender(_ =>
-                Fragment(
-                  Typography.variant(Style.h4).gutterBottom(true).component("h2")("Customization"),
-                  DarkTheme(),
-                  Palette()
+            ),
+            router.Route(
+              RouteProps()
+                .setPath("/customization")
+                .setRender(_ =>
+                  Fragment(
+                    Typography.variant(Style.h4).gutterBottom(true).component("h2")("Customization"),
+                    DarkTheme(),
+                    Palette()
+                  )
                 )
-              )
+            )
           )
         )
       )

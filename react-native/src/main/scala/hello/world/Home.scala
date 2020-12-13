e360a18de46adb07c37b8f1b124b53da056f580a
Mario Càllisto <mario.callisto@gmail.com>
2020-12-13 16:27:46 +0100

    Added example of ReactNative.Linking

--- react-native/src/main/scala/hello/world/Home.scala
+++ react-native/src/main/scala/hello/world/Home.scala
@@ -3,9 +3,13 @@ package hello.world
 import slinky.core.FunctionalComponent
 import slinky.core.annotations.react
 import slinky.native.{Image, Text, View}
+import typings.antDesignReactNative.components.Button
+import typings.antDesignReactNative.{antDesignReactNativeStrings => antdStrings}
 import typings.expoConstants.mod.default.{expoVersion, statusBarHeight}
+import typings.expoLinking.mod.openURL
 import typings.reactNative.mod.{TextStyle, ViewStyle}
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 
@@ -18,6 +22,9 @@ object ScalaLogo extends js.Object
 
   val component = FunctionalComponent[Props] {
     case () =>
+
+      def linking = openURL("https://scalablytyped.org").toFuture
+
       View(style = ViewStyle().setMargin(20))(
         Text(style = TextStyle().setFontSize(16))(
           "This is a demo written in Scala through Scala.js, Slinky, ScalablyTyped and Expo.\n\n" +
@@ -25,6 +32,9 @@ object ScalaLogo extends js.Object
         ),
         Text(s"The status bar is ${statusBarHeight} pixels high."),
         Text(s"Expo version is ${expoVersion}."),
+        Button("Open URL with ReactNative.Linking")
+          .`type`(antdStrings.primary)
+          .onPress(_ => linking.foreach(_ => ())),
         Text(s"This image is a local asset:"),
         Image(source = ScalaLogo)
       )
